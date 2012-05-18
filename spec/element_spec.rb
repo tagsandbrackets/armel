1cdb0cc7498e05c0140a4070fc9beb2deef33ebe
Omar Rodriguez <omarjavier15@gmail.com>
2012-05-18 15:11:39 +0000

    Refactor: Specs use specify sintax Replace subject sintax with specify Subject was producing an error with no trace

--- spec/element_spec.rb
+++ spec/element_spec.rb
@@ -3,38 +3,32 @@ require 'spec_helper'
 describe Element do
   context "#create" do
     context "simple element" do
-      subject { Element.new(:mytag).to_s }
-      it { should == "<mytag />" }
+      specify { Element.new(:mytag).to_s.should == "<mytag />" }
 
       context "with attributes" do
-        subject { Element.new(:div, id: 'foo', class: 'bar').to_s }
-        it { should == "<div id='foo' class='bar' />" }
+        specify do 
+          Element.new(:div, id: 'foo', class: 'bar').to_s.should == "<div id='foo' class='bar' />"
+        end
       end
     end
 
     context "with text" do
       context "explicit sintax" do
-        subject do
-          Element.new(:h1) { self << "Hello World" }.to_s
+        specify do 
+          Element.new(:h1) { self << "Hello World" }.to_s.should == "<h1>Hello World</h1>"
         end
-
-        it { should == "<h1>Hello World</h1>" }
       end
     end
 
     context "nested element" do
-      subject do 
-        Element.new(:with_nested) { nested }.to_s
+      specify do 
+        Element.new(:with_nested) { nested }.to_s.should == "<with_nested><nested /></with_nested>"
       end
-
-      it { should == "<with_nested><nested /></with_nested>" }
       
       context "with attributes" do
-        subject do 
-          Element.new(:form) { input id: 'foo', class: 'bar' }.to_s
+        specify do 
+          Element.new(:form) { input id: 'foo', class: 'bar' }.to_s.should == "<form><input id='foo' class='bar' /></form>"
         end
-        
-        it { should == "<form><input id='foo' class='bar' /></form>" }
       end
     end
   end
