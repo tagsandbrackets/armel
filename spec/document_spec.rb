b863eb57257eadcd6a934ea9f3cf382059dd8f89
Omar Rodriguez <omarjavier15@gmail.com>
2012-05-18 16:47:06 +0000

    Spec simplified

--- spec/document_spec.rb
+++ spec/document_spec.rb
@@ -2,52 +2,38 @@ require 'spec_helper'
 
 describe RML::Document do
   let(:doc) { RML::Document }
-
-  context "#create" do
-    context "simple element" do
-      specify do
-        doc.string {
-          mytag
-        }.should == "<mytag />"
-      end
-
-      context "with attributes" do
-        specify do
-          doc.string {
-            div id: 'foo', class: 'bar'
-          }.should == "<div id='foo' class='bar' />" 
-        end
-      end
-    end
-
-    context "with text" do
-      context "explicit sintax" do
-        specify do
-          doc.string {
-            h1 { self << "Hello World" }
-          }.should == "<h1>Hello World</h1>"
-        end
-      end
-    end
-
-    context "nested element" do
-      specify do
-        doc.string {
-          with_nested { 
-            nested 
-          }
-        }.should == "<with_nested><nested /></with_nested>"
-      end
-      
-      context "with attributes" do
-        specify do
-          doc.string {
-            form { 
-              input id: 'foo', class: 'bar' 
-            } 
-          }.should == "<form><input id='foo' class='bar' /></form>"
-        end
-      end
-    end
+  
+  it "should define empty elements" do
+    doc.string {
+      mytag
+    }.should == "<mytag />"
   end
+  
+  it "should define empty elements with attributes" do
+    doc.string {
+      div id: 'foo', class: 'bar'
+    }.should == "<div id='foo' class='bar' />" 
+  end
+  
+  it "should define elements with text using explicit sintax" do
+    doc.string {
+      h1 { self << "Hello World" }
+    }.should == "<h1>Hello World</h1>"
+  end
+  
+  it "should define nested elements" do
+    doc.string {
+      with_nested { 
+        nested 
+      }
+    }.should == "<with_nested><nested /></with_nested>"
+  end
+  
+  it "should define nested elements with attributes" do
+    doc.string {
+      form { 
+        input id: 'foo', class: 'bar' 
+      } 
+    }.should == "<form><input id='foo' class='bar' /></form>"
+  end 
 end
