f34544151fb41b918d53fbf95c866c238e9d4971
Rafael George <george.rafael@gmail.com>
2012-05-18 13:18:00 +0000

    fixed failing spec

--- spec/element_spec.rb
+++ spec/element_spec.rb
@@ -5,30 +5,26 @@ describe Element do
     context "simple element" do
       subject { Element.new(:mytag).to_s }
       it { should == "<mytag />" }
-      
+
       context "with attributes" do
         subject { Element.new(:div, id: 'foo', class: 'bar').to_s }
         it { should == "<div id='foo' class='bar' />" }
       end
     end
-    
+
     context "with text" do
       subject do
-        Element.new(:h1) {
-          self << "Hello World"
-        }
+        Element.new(:h1) { self << "Hello World" }.to_s
       end
-      
+
       it { should == "<h1>Hello World</h1>" }
     end
-    
+
     context "nested element" do
       subject do 
-        Element.new(:with_nested) {
-          nested
-        }.to_s
+        Element.new(:with_nested) { nested }.to_s
       end
-      
+
       it { should == "<with_nested><nested /></with_nested>" }
     end    
   end
