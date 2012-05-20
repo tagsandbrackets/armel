f73d437ae922a8db0307fc336b4bcfece9008bc0
Omar Rodriguez <omarjavier15@gmail.com>
2012-05-20 12:24:08 +0000

    Refactor: Attributes spec use rspec context

--- spec/attributes_spec.rb
+++ spec/attributes_spec.rb
@@ -3,22 +3,35 @@ require 'spec_helper'
 describe RML::Attributes do
   let(:attrs) { RML::Attributes }  
   
-  it "should be able to build attributes" do
-    attrs.string(id: 'foo', title: 'bar').should == "id='foo' title='bar'"
-  end
-  
-  it "should be able to build numeric attributes" do
-    attrs.string(num: 1, text: 'foo').should == "num=1 text='foo'"
-  end
-  
-  it "should ignore nil values when building attributes" do
-    attrs.string(id: nil, text: 'foo').should == "text='foo'"
-  end
-  
-  it "should be able to build attributes with namespace" do
-    attrs.string(xmlns: { 
-      h: "http://www.w3.org/TR/html4/", 
-      r: "http://www.2hf.com/rml/" 
-    }).should == "xmlns:h='http://www.w3.org/TR/html4/' xmlns:r='http://www.2hf.com/rml/'"
+  context "Build attributes string" do
+    specify "Attributes list" do
+      attrs.string(id: 'foo', title: 'bar').should == "id='foo' title='bar'"
+    end
+    
+    specify "Numeric attributes" do
+      attrs.string(num: 1, text: 'foo').should == "num=1 text='foo'"
+    end
+    
+    specify "Ignore nil values" do
+      attrs.string(id: nil, text: 'foo').should == "text='foo'"
+    end
+    
+    context "With namespace" do
+      specify "Single" do
+        attrs.string(xmlns: { 
+          h: "http://www.w3.org/TR/html4/", 
+          r: "http://www.2hf.com/rml/" 
+        }).should == "xmlns:h='http://www.w3.org/TR/html4/' xmlns:r='http://www.2hf.com/rml/'"
+      end
+      
+      specify "Multiple" do
+        attrs.string(h: { 
+          id: 1, 
+          class: 'foo' 
+        }, rml: {
+          test: 'yeah'
+        }).should == "h:id=1 h:class='foo' rml:test='yeah'"
+      end
+    end
   end
 end
