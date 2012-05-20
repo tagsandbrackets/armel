f3b1090b3eb75f307c4d6e0abed3f9fecfab375d
Omar Rodriguez <omarjavier15@gmail.com>
2012-05-20 12:58:58 +0000

    Refactor Document spec to use rspec context

--- spec/document_spec.rb
+++ spec/document_spec.rb
@@ -3,66 +3,77 @@ require 'spec_helper'
 describe RML::Document do
   let(:doc) { RML::Document }
   
-  it "should define empty elements" do
-    doc.string {
-      mytag
-    }.should == "<mytag />"
+  context "Single elmenet" do
+    specify "Empty" do
+      doc.string {
+        mytag
+      }.should == "<mytag />"
+    end
+    
+    specify "With attributes" do
+      doc.string {
+        div id: 'foo', class: 'bar'
+      }.should == "<div id='foo' class='bar' />" 
+    end
+    
+    context "With text" do
+      context "Explicit sintax" do
+        specify "Explicit sintax" do
+          doc.string {
+            h1 { self << "Hello World" }
+          }.should == "<h1>Hello World</h1>"
+        end
+      end
+      
+      context "Short sintax" do
+        specify "Short sintax" do
+          doc.string {
+            h1 "Hello World"
+          }.should == "<h1>Hello World</h1>"
+        end
+        
+        specify "And attributes" do
+          doc.string {
+            h1 "Hello World", id: 'foo', class: 'bar'
+          }.should == "<h1 id='foo' class='bar'>Hello World</h1>"
+        end
+      end
+    end
   end
   
-  it "should define empty elements with attributes" do
-    doc.string {
-      div id: 'foo', class: 'bar'
-    }.should == "<div id='foo' class='bar' />" 
+  context "Nest elements" do
+    specify "Empty element" do
+      doc.string {
+        with_nested { 
+          nested 
+        }
+      }.should == "<with_nested><nested /></with_nested>"
+    end
+
+    specify "Surrounded by text" do
+      doc.string {
+        sandwitch { 
+          self << "bread"
+          ham
+          self << "bread"
+        }
+      }.should == "<sandwitch>bread<ham />bread</sandwitch>"
+    end
+    
+    specify "With attributes and nested element" do
+      doc.string {
+        with_nested(id: 'foo') { 
+          nested 
+        }
+      }.should == "<with_nested id='foo'><nested /></with_nested>"
+    end
+    
+    specify "With attributes on the parent" do
+      doc.string {
+        form { 
+          input id: 'foo', class: 'bar' 
+        } 
+      }.should == "<form><input id='foo' class='bar' /></form>"
+    end  
   end
-  
-  it "should define elements with text using explicit sintax" do
-    doc.string {
-      h1 { self << "Hello World" }
-    }.should == "<h1>Hello World</h1>"
-  end
-  
-  it "should define elements with text using short sintax" do
-    doc.string {
-      h1 "Hello World"
-    }.should == "<h1>Hello World</h1>"
-  end
-  
-  it "should define elements with text in outer and inner tag" do
-    doc.string {
-      div { 
-        self << "Hello"
-        h1 { self << "World" }
-      }
-    }.should == "<div>Hello<h1>World</h1></div>"
-  end
-  
-  it "should define elements with attributes and text using short sintax" do
-    doc.string {
-      h1 "Hello World", id: 'foo', class: 'bar'
-    }.should == "<h1 id='foo' class='bar'>Hello World</h1>"
-  end
-  
-  it "should define nested elements" do
-    doc.string {
-      with_nested { 
-        nested 
-      }
-    }.should == "<with_nested><nested /></with_nested>"
-  end
-  
-  it "should define an element with attributes and nested element" do
-    doc.string {
-      with_nested(id: 'foo') { 
-        nested 
-      }
-    }.should == "<with_nested id='foo'><nested /></with_nested>"
-  end
-  
-  it "should define nested elements with attributes" do
-    doc.string {
-      form { 
-        input id: 'foo', class: 'bar' 
-      } 
-    }.should == "<form><input id='foo' class='bar' /></form>"
-  end  
 end
