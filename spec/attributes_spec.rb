6f01472d313b48847de1986b4b8719385b86371c
Omar Rodriguez <omarjavier15@gmail.com>
2012-05-20 11:09:52 +0000

    Rename list var to attrs in attributes_spec

--- spec/attributes_spec.rb
+++ spec/attributes_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 describe RML::Attributes do
-  let(:list) { RML::Attributes }  
+  let(:attrs) { RML::Attributes }  
   
   it "should be able to build an attributes list string" do
-    list.string(id: 'foo', title: 'bar').should == "id='foo' title='bar'"
+    attrs.string(id: 'foo', title: 'bar').should == "id='foo' title='bar'"
   end
 end
