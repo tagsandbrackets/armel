7cbd3e32d5b58e86e3dacefe4f328bcb150021d7
Omar Rodriguez <omarjavier15@gmail.com>
2012-05-20 18:35:40 +0000

    Rename document_spec.rb to element_spec.rb Add new document_spec.rb for specify RML::Document responsabilities

--- spec/document_spec.rb
+++ spec/document_spec.rb
@@ -3,79 +3,22 @@ require 'spec_helper'
 describe RML::Document do
   let(:doc) { RML::Document }
   
-  context "Single elmenet" do
-    specify "Empty" do
-      doc.string {
-        mytag
-      }.should == "<mytag />"
-    end
-    
-    context "With attributes" do
-      specify "With attributes" do
-        doc.string {
-          div id: 'foo', class: 'bar'
-        }.should == "<div id='foo' class='bar' />" 
-      end
-      
-      specify "And nested element" do
-        doc.string {
-          with_nested(id: 'foo') { 
-            nested 
-          }
-        }.should == "<with_nested id='foo'><nested /></with_nested>"
-      end
-    end
-    
-    context "With text" do
-      context "Explicit sintax" do
-        specify "Explicit sintax" do
-          doc.string {
-            h1 { self << "Hello World" }
-          }.should == "<h1>Hello World</h1>"
-        end
-      end
-      
-      context "Short sintax" do
-        specify "Short sintax" do
-          doc.string {
-            h1 "Hello World"
-          }.should == "<h1>Hello World</h1>"
-        end
-        
-        specify "And attributes" do
-          doc.string {
-            h1 "Hello World", id: 'foo', class: 'bar'
-          }.should == "<h1 id='foo' class='bar'>Hello World</h1>"
-        end
-      end
-    end
+  specify "Empty" do
+    doc.string.should == ''
   end
   
-  context "Nest elements" do
-    specify "Empty element" do
+  context "With elements" do
+    specify "Single" do
       doc.string {
-        with_nested { 
-          nested 
-        }
-      }.should == "<with_nested><nested /></with_nested>"
-    end
-
-    specify "Surrounded by text" do
-      doc.string {
-        sandwitch { 
-          self << "bread"
-          ham
-          self << "bread"
-        }
-      }.should == "<sandwitch>bread<ham />bread</sandwitch>"
+        xml
+      }.should == "<xml />"
     end
     
-    specify "With attributes" do
+    specify "Multiple" do
       doc.string {
-        form { 
-          input id: 'foo', class: 'bar' 
-        } 
-      }.should == "<form><input id='foo' class='bar' /></form>"
-    end  
+        one
+        two
+      }.should == "<one /><two />"
+    end
   end
 end
---
+++ spec/element_spec.rb
@@ -0,0 +1,81 @@
+require 'spec_helper'
+
+describe RML::Element do
+  let(:doc) { RML::Document }
+  
+  context "Single elmenet" do
+    specify "Empty" do
+      doc.string {
+        mytag
+      }.should == "<mytag />"
+    end
+    
+    context "With attributes" do
+      specify "With attributes" do
+        doc.string {
+          div id: 'foo', class: 'bar'
+        }.should == "<div id='foo' class='bar' />" 
+      end
+      
+      specify "And nested element" do
+        doc.string {
+          with_nested(id: 'foo') { 
+            nested 
+          }
+        }.should == "<with_nested id='foo'><nested /></with_nested>"
+      end
+    end
+    
+    context "With text" do
+      context "Explicit sintax" do
+        specify "Explicit sintax" do
+          doc.string {
+            h1 { self << "Hello World" }
+          }.should == "<h1>Hello World</h1>"
+        end
+      end
+      
+      context "Short sintax" do
+        specify "Short sintax" do
+          doc.string {
+            h1 "Hello World"
+          }.should == "<h1>Hello World</h1>"
+        end
+        
+        specify "And attributes" do
+          doc.string {
+            h1 "Hello World", id: 'foo', class: 'bar'
+          }.should == "<h1 id='foo' class='bar'>Hello World</h1>"
+        end
+      end
+    end
+  end
+  
+  context "Nest elements" do
+    specify "Empty element" do
+      doc.string {
+        with_nested { 
+          nested 
+        }
+      }.should == "<with_nested><nested /></with_nested>"
+    end
+
+    specify "Surrounded by text" do
+      doc.string {
+        sandwitch { 
+          self << "bread"
+          ham
+          self << "bread"
+        }
+      }.should == "<sandwitch>bread<ham />bread</sandwitch>"
+    end
+    
+    specify "With attributes" do
+      doc.string {
+        form { 
+          input id: 'foo', class: 'bar' 
+        } 
+      }.should == "<form><input id='foo' class='bar' /></form>"
+    end  
+  end
+end
