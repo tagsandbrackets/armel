7c52e268d145460cdb2954053155f2177f034ca1
Omar Rodriguez <omarjavier15@gmail.com>
2012-05-20 10:43:37 +0000

    Refactor ElementArgs class Preserves the original arguments

--- lib/rml/element_args.rb
+++ lib/rml/element_args.rb
@@ -1,14 +1,28 @@
 module RML
   class ElementArgs
-    attr_reader :attributes, :text
-    
     def initialize(*args)
-      @attributes = args.find { |a| a.is_a? Hash } || {}
-      @text = args.find { |a| a.is_a? String }
+      @args = args
+    end
+
+    def attributes
+      @attributes ||= find_attributes
+    end
+    
+    def text
+      @text ||= find_text
     end
     
     def has_text?
-      !@text.nil?
+      !text.nil?
     end
+    
+    private
+      def find_attributes
+        @args.find { |a| a.is_a? Hash } || {}
+      end
+      
+      def find_text
+        @args.find { |a| a.is_a? String }
+      end
   end
 end
