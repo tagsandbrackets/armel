2723667b872582d25a5f7e9888f0399a2e7aeb81
Omar Rodriguez <omarjavier15@gmail.com>
2012-05-20 14:05:37 +0000

    Refactor: Move ElementArgs inside Factory Rename it to Args

--- lib/rml/element/factory.rb
+++ lib/rml/element/factory.rb
@@ -3,9 +3,36 @@ module RML
     class Factory
       class << self
         def create(name, *args, &block)
-          elem_args = ElementArgs.new *args
+          elem_args = Args.new *args
           Element.new(name, elem_args.attributes, elem_args.text, &block)
         end
+      end
+      
+      class Args
+        def initialize(*args)
+          @args = args
+        end
+
+        def attributes
+          @attributes ||= RML::Attributes.new find_attributes
+        end
+        
+        def text
+          @text ||= find_text
+        end
+        
+        def has_text?
+          !text.nil?
+        end
+        
+        private
+          def find_attributes
+            @args.find { |a| a.is_a? Hash } || {}
+          end
+          
+          def find_text
+            @args.find { |a| a.is_a? String }
+          end
       end
     end
   end
--- lib/rml/element_args.rb
+++ lib/rml/element_args.rb
@@ -1,28 +1,3 @@
 module RML
-  class ElementArgs
-    def initialize(*args)
-      @args = args
-    end
-
-    def attributes
-      @attributes ||= RML::Attributes.new find_attributes
-    end
-    
-    def text
-      @text ||= find_text
-    end
-    
-    def has_text?
-      !text.nil?
-    end
-    
-    private
-      def find_attributes
-        @args.find { |a| a.is_a? Hash } || {}
-      end
-      
-      def find_text
-        @args.find { |a| a.is_a? String }
-      end
-  end
+  
 end
