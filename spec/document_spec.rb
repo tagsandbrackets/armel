12dbc7316a0c9ea55933b0817878cd69fe61901a
Rafael George <george.rafael@gmail.com>
2012-05-21 17:52:53 +0000

    Rename string method in Document, define make more sense

--- spec/document_spec.rb
+++ spec/document_spec.rb
@@ -2,18 +2,18 @@ require 'spec_helper'
 
 describe RML::Document do
   specify "Empty" do
-    described_class.string.should == ''
+    described_class.define.should == ''
   end
   
   context "With elements" do
     specify "Single" do
-      described_class.string {
+      described_class.define {
         xml
       }.should == "<xml />"
     end
     
     specify "Multiple" do
-      described_class.string {
+      described_class.define {
         one
         two
       }.should == "<one /><two />"
